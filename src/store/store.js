24d2df983acf6c185ab1da25af77a31b18b6e01e
xuniorss <fortunatojunior2020@gmail.com>
2023-03-17 17:56:40 -0300

    aula 183 - Redux Toolkit Intro

--- src/store/store.js
+++ src/store/store.js
@@ -1,34 +1,34 @@
-import { compose, createStore, applyMiddleware } from 'redux';
-import { persistStore, persistReducer } from 'redux-persist';
-import storage from 'redux-persist/lib/storage';
-import logger from 'redux-logger';
+import { configureStore } from '@reduxjs/toolkit'
+// import { rootReducer } from 'redux'
+import logger from 'redux-logger'
 
-import { rootReducer } from './root-reducer';
+// import { rootReducer } from 'redux-persist'
+// import storage from 'redux-persist/lib/storage'
 
-const middleWares = [process.env.NODE_ENV === 'development' && logger].filter(
-  Boolean
-);
+import { rootReducer } from './root-reducer'
 
-const composeEnhancer =
-  (process.env.NODE_ENV !== 'production' &&
-    window &&
-    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
-  compose;
+const middleWares = [process.env.NODE_ENV === 'development' && logger].filter(Boolean)
 
-const persistConfig = {
-  key: 'root',
-  storage,
-  blacklist: ['user'],
-};
+// const composeEnhancer =
+//    (process.env.NODE_ENV !== 'production' &&
+//       window &&
+//       window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
+//    compose
 
-const persistedReducer = persistReducer(persistConfig, rootReducer);
+// const persistConfig = {
+//    key: 'root',
+//    storage,
+//    blacklist: ['user'],
+// }
 
-const composedEnhancers = composeEnhancer(applyMiddleware(...middleWares));
+// const persistedReducer = persistReducer(persistConfig, rootReducer)
 
-export const store = createStore(
-  persistedReducer,
-  undefined,
-  composedEnhancers
-);
+// const composedEnhancers = composeEnhancer(applyMiddleware(...middleWares))
 
-export const persistor = persistStore(store);
+// export const store = createStore(persistedReducer, undefined, composedEnhancers)
+
+// export const persistor = persistStore(store)
+export const store = configureStore({
+   reducer: rootReducer,
+   // middleware: middleWares,
+})
