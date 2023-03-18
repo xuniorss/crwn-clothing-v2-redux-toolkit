7ac9012c58f2578bd4c7f9a96778b23f402df259
xuniorss <fortunatojunior2020@gmail.com>
2023-03-18 13:05:41 -0300

    aula 188 - Categories to createSlice

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,30 +1,30 @@
-import { useEffect } from 'react';
-import { Routes, Route } from 'react-router-dom';
-import { useDispatch } from 'react-redux';
+import { useEffect } from 'react'
+import { useDispatch } from 'react-redux'
+import { Route, Routes } from 'react-router-dom'
 
-import CategoriesPreview from '../categories-preview/categories-preview.component';
-import Category from '../category/category.component';
-import { getCategoriesAndDocuments } from '../../utils/firebase/firebase.utils';
-import { setCategories } from '../../store/categories/category.action';
+import { setCategories } from '../../store/categories/category.reducer'
+import { getCategoriesAndDocuments } from '../../utils/firebase/firebase.utils'
+import CategoriesPreview from '../categories-preview/categories-preview.component'
+import Category from '../category/category.component'
 
 const Shop = () => {
-  const dispatch = useDispatch();
+   const dispatch = useDispatch()
 
-  useEffect(() => {
-    const getCategoriesMap = async () => {
-      const categoriesArray = await getCategoriesAndDocuments('categories');
-      dispatch(setCategories(categoriesArray));
-    };
+   useEffect(() => {
+      const getCategoriesMap = async () => {
+         const categoriesArray = await getCategoriesAndDocuments('categories')
+         dispatch(setCategories(categoriesArray))
+      }
 
-    getCategoriesMap();
-  }, []);
+      getCategoriesMap()
+   }, [])
 
-  return (
-    <Routes>
-      <Route index element={<CategoriesPreview />} />
-      <Route path=':category' element={<Category />} />
-    </Routes>
-  );
-};
+   return (
+      <Routes>
+         <Route index element={<CategoriesPreview />} />
+         <Route path=":category" element={<Category />} />
+      </Routes>
+   )
+}
 
-export default Shop;
+export default Shop
