31ee85b4c1f4c0527e98946c571c625ac3c12952
xuniorss <fortunatojunior2020@gmail.com>
2023-03-19 15:36:41 -0300

    aula 195 - Finishing Stripe Payment

--- src/components/payment-form/payment-form.component.jsx
+++ src/components/payment-form/payment-form.component.jsx
@@ -10,11 +10,40 @@ const PaymentForm = () => {
       e.preventDefault()
 
       if (!stripe || !elements) return
+
+      const response = await fetch('/.netlify/functions/create-payment-intent', {
+         method: 'POST',
+         headers: {
+            'Content-Type': 'application/json',
+         },
+         body: JSON.stringify({ amount: 10000 }),
+      }).then((res) => res.json())
+
+      const {
+         paymentIntent: { client_secret },
+      } = response
+
+      const paymentResult = await stripe.confirmCardPayment(client_secret, {
+         payment_method: {
+            card: elements.getElement(CardElement),
+            billing_details: {
+               name: 'Junior',
+            },
+         },
+      })
+
+      if (paymentResult.error) {
+         alert(paymentResult.error)
+      } else {
+         if (paymentResult.paymentIntent.status === 'succeeded') {
+            alert('Payment Successful')
+         }
+      }
    }
 
    return (
       <PaymentFormContainer>
-         <FormContaier>
+         <FormContaier onSubmit={paymentHandler}>
             <h2>Credit Card Payment: </h2>
             <CardElement />
             <Button buttonType={BUTTON_TYPE_CLASSES.inverted}>Pay now</Button>
